ae37d7393031e4bc2fbccd8bba2aa172dcad0072
Alejandro Patino <apatino@seedem.co>
2019-05-22 17:56:05 -0500

    Creacion de plugin

--- code/web/modules/custom/noticias/src/Plugin/Block/NoticiasSimpleBlock.php
+++ code/web/modules/custom/noticias/src/Plugin/Block/NoticiasSimpleBlock.php
@@ -1,7 +1,38 @@
 <?php
 
+/**
+ * @file
+ * Contains \Drupal\noticias\plugin\Block\NoticiasSimpleBlock
+ */
+
 namespace Drupal\noticias\Plugin\Block; 
 
-class NoticiasSimpleBlock {
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * @Block (
+ *  id = "noticias_simple",
+ *  admin_label = @Translation("Bloque de noticias")
+ * )
+ */
+
+class NoticiasSimpleBlock extends BlockBase {
+
+    public function defaultConfiguration() {
+        return array(
+            'noticias_block_string' => $this->t('Valor por defecto. Este bloque se creo a las %time', array('%time' => date('c'))),
+        );
+    }
+
+    /**
+     * 
+     */
+
+    public function build() {
+        return array(
+            '#type' => 'markup',
+            '#markup' => $this->configuration['noticias_block_string'],
+        );
+    }
 
 }
